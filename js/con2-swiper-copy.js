41429bbc077dd0fced3c950351a19d92413e20fe
CircleYoo <galleon32122@naver.com>
2022-12-20 15:55:20 +0900

    first

---
+++ js/con2-swiper-copy.js
@@ -0,0 +1,42 @@
+(function() {
+    'use strict';
+
+    const swiperList = [
+        '.main_swiper', '.sub_swiper2', '.sub_swiper3', '.sub_swiper4', '.sub_swiper5', '#con2_productNm'
+    ];
+	const swiperObjList = [];
+
+	swiperList.forEach(item => {
+		const swiper = new Swiper(`${item} .swiper-container`, {
+			navigation: {
+			  nextEl: '.swiper-button-next',
+			  prevEl: '.swiper-button-prev',
+			},
+			loop: true
+		});
+		
+		swiperObjList.push(swiper);
+		
+        let isTouch = false;
+		swiper.on('slideChange', function() {
+            if (!isTouch) {
+                isTouch = false;
+                return;
+            }
+            isTouch = false;
+			swiperObjList.forEach(obj => {
+				if(swiper !== obj) {					
+					if(swiper.swipeDirection === 'prev') {
+						obj.slidePrev();						
+					} else {
+						obj.slideNext();
+					}
+				}
+			})		
+			
+		});
+		swiper.on('touchEnd', function() {
+            isTouch = true;
+		});
+	});
+})();
